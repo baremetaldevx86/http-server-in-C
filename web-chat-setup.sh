557ffaa973ca608f396c01fec69408b853d01730
Vedant Chauhan <123994504+baremetaldevx86@users.noreply.github.com>
2026-01-28 20:54:15 +0530

    Fix compilation command to use chat-server.c

--- web-chat-setup.sh
+++ web-chat-setup.sh
@@ -6,7 +6,7 @@ echo "========================"
 
 # Compile the web server
 echo "Compiling web chat server..."
-gcc -o web_chat_server web-server.c -lpthread
+gcc -o web_chat_server chat-server.c -lpthread
 
 if [ $? -ne 0 ]; then
     echo "Failed to compile server. Make sure you have gcc and pthread library."
